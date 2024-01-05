ca9aab328e469af553801da27ec667637fe634cd
Caio Souza <caiobsouzaa@gmail.com>
2024-01-05 14:16:59 -0300

    fix: entry point

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
       {
         test: /\.ts$/,
         loader: "ts-loader",
-        exclude: /node_modules/,
+        exclude: /node_modules|scripts/,
       },
     ],
   },
