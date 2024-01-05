e59ac33155392ee9040b269e157fc0f1ad3078de
Caio Souza <caiobsouzaa@gmail.com>
2024-01-05 13:06:38 -0300

    build: add bumped package.json

--- scripts/version.ts
+++ scripts/version.ts
@@ -34,5 +34,5 @@ async function askVersion() {
 
 (async () => {
   const version = await askVersion();
-  await asyncExec(`npm version ${version} --no-git-tag-version`);
+  await asyncExec(`npm version ${version} --no-git-tag-version && git add ./package*`);
 })();
