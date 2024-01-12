083e29a9ac901824a8ddb375c7e4973bdef4c9da
Caio Souza <caiobsouzaa@gmail.com>
2024-01-11 22:02:38 -0300

    build: remove unnecessary files from dist

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ import { compilerOptions } from "./tsconfig.json";
 const jestConfig: JestConfigWithTsJest = {
   preset: "ts-jest",
   roots: ["<rootDir>"],
-  modulePaths: [compilerOptions.baseUrl], // <-- This will be set to 'baseUrl' value
+  // modulePaths: [compilerOptions.baseUrl], // <-- This will be set to 'baseUrl' value
   moduleDirectories: ["node_modules", "<rootDir>"],
   // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths), // Uncomment if the package uses path aliases
   setupFiles: ["./jest.setup.ts"],
