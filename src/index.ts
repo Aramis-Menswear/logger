867e42b58b10a290c1921c55e27970604cbfbaec
Caio Souza <caiobsouzaa@gmail.com>
2024-01-11 22:07:36 -0300

    fix: bypass winston

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,54 @@
-import { logger } from './config';
-
-const ERROR_EMOJI = "⛔";
-const WARNING_EMOJI = "⚠️";
-const INFO_EMOJI = "▶️";
-const SUCCESS_EMOJI = "✅";
+const ERROR_EMOJI = '⛔';
+const WARNING_EMOJI = '⚠️';
+const INFO_EMOJI = '▶️';
+const SUCCESS_EMOJI = '✅';
 
 const logConsole = (...msgs: unknown[]) => {
   msgs.forEach((msg: unknown) => {
-    logger.info(">> ", msg);
+    console.log('>> ', msg);
   });
-  logger.info("");
-  console.log("OK");
+  console.log('');
 };
 
-const info = (...messages: unknown[]) => {
-  logger.info(`${INFO_EMOJI} INFORMATION LOGS`)
-  messages.forEach((message: unknown) => {
-    logger.info(message);
-  });
+const info = (...msgs: unknown[]) => {
+  console.log('');
+  console.groupCollapsed(
+    '\x1b[36m%s\x1b[0m',
+    `${INFO_EMOJI}  INFORMATION LOGS`,
+  );
+  logConsole(...msgs);
+  console.groupEnd();
 };
 
 const warn = (...msgs: unknown[]) => {
-  console.log("");
-  console.groupCollapsed("\x1b[33m%s\x1b[0m", `${WARNING_EMOJI} WARNING LOGS`);
+  console.log('');
+  console.groupCollapsed('\x1b[33m%s\x1b[0m', `${WARNING_EMOJI} WARNING LOGS`);
   logConsole(...msgs);
   console.groupEnd();
 };
 
 const error = (...msgs: unknown[]) => {
-  console.log("");
-  console.groupCollapsed("\x1b[31m%s\x1b[0m", `${ERROR_EMOJI} ERROR LOGS`);
+  console.log('');
+  console.groupCollapsed('\x1b[31m%s\x1b[0m', `${ERROR_EMOJI} ERROR LOGS`);
   logConsole(...msgs);
   console.groupEnd();
 };
 
 const success = (...msgs: unknown[]) => {
-  console.log("");
-  console.groupCollapsed("\x1b[32m%s\x1b[0m", `${SUCCESS_EMOJI} SUCCESS LOGS`);
+  console.log('');
+  console.groupCollapsed('\x1b[32m%s\x1b[0m', `${SUCCESS_EMOJI} SUCCESS LOGS`);
   logConsole(...msgs);
   console.groupEnd();
 };
 
-export const log = {
+const log = {
   info,
   warn,
   error,
   success,
 };
 
+export default log;
 
 export const logFunctionExportedForTesting = {
   logConsole,
